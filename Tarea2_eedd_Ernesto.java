0480c0e09fa9a4f2ffe08357b88ff4a353b02c78
Ernesto Ruiz <a_ernesto.ruiz.fernandez@iespablopicasso.es>
2021-11-18 14:20:36 +0100

    Añado el archivo .java al repositorio

---
+++ Tarea2_eedd_Ernesto.java
@@ -0,0 +1,14 @@
+package Tarea2;
+
+public class Tarea2_eedd_Ernesto {
+	public static void main(String[] args) {
+		int m [] = new int[20];
+		
+		for (int j = 0; j < m.length; j++) {
+			m[j] = (int)(Math.random()*10);
+			System.out.print(m[j]);
+			System.out.print(", ");
+		}
+		
+	}
+}
