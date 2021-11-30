d3563fa8517c79987a192116f64475c0653c72af
Ernesto Ruiz <a_ernesto.ruiz.fernandez@iespablopicasso.es>
2021-11-30 12:21:16 +0100

    Creo un metodo para imprimir por pantalla el numero de veces que aparece en el array el numero introducido por teclado

--- Tarea2_eedd_Ernesto.java
+++ Tarea2_eedd_Ernesto.java
@@ -1,13 +1,25 @@
 package Tarea2;
 
+import java.util.Scanner;
+
 public class Tarea2_eedd_Ernesto {
 	public static void main(String[] args) {
 		int m [] = new int[20];
+		int x;
+		int y = 0;
+		
+		Scanner s = new Scanner(System.in);
+		System.out.println("Dime el número que quieres comprobar");
+		s = Integer.parseInt(s.nextLine());
 		
 		for (int j = 0; j < m.length; j++) {
 			m[j] = (int)(Math.random()*10);
 			System.out.print(m[j]);
 			System.out.print(", ");
+			if(m[j] = x) {
+				y++;
+			}
 		}
+		System.out.println("El número seleccionado está repetido: " + y + " veces en el array");
 	}
 }
