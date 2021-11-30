8fd11fa934ab8cbfa44d202a700d64542c802384
Ernesto Ruiz <a_ernesto.ruiz.fernandez@iespablopicasso.es>
2021-11-30 12:09:32 +0100

    Creo el array con 20 números aleatorios del 1 al 10

--- Tarea2_eedd_Ernesto.java
+++ Tarea2_eedd_Ernesto.java
@@ -4,15 +4,7 @@ public class Tarea2_eedd_Ernesto {
 	public static void main(String[] args) {
 		int m [] = new int[20];
 		
-		for (int j = 0; j < m.length; j++) {
-			m[j] = (int)(Math.random()*10);
-			System.out.print(m[j]);
-			System.out.print(", ");
 		
-		}
-		for (int i = 0; i < m.length; i++) {
-			System.out.print(m[i]);
-			System.out.print(",");
 		}
 		
 	}
